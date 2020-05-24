5b283ef37cbc4849451fb24121e79337e98282f6
ss-designs <design@suzistranahan.com>
2020-05-24 09:33:06 -0700

    Added recent work sections with pods

--- wp-content/themes/bb-theme-child/functions.php
+++ wp-content/themes/bb-theme-child/functions.php
@@ -9,3 +9,14 @@ require_once 'classes/class-fl-child-theme.php';
 
 // Actions
 add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+
+// Change Pods MORE FIELDS Metabox Title to Custom
+
+add_filter('pods_meta_default_box_title','ni_changethatname',10,5);
+
+function ni_changethatname($title, $pod, $fields, $type, $name ) {
+
+// assuming we are changing the meta box title on a pod named 'business' 
+  $title = ($name=='project') ? __('project info', 'plugin_lang') : $title ;
+  return $title;
+}
